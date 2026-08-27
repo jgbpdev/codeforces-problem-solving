5412cb48b4cf530dccb5786fd5897191ec02ce3e
jgbpdev <jgbpurification@gmail.com>
2026-08-28 01:02:14 +0600

    Add files via upload

---
+++ 800/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    int wordlength;
+
+    cin>>wordlength;
+
+    while (wordlength--)
+    {
+        
+        string word;
+        cin>>word;
+
+
+        if(word.length()>10){
+
+            cout<<word[0]<<word.length()-2<<word[word.length()-1]<<endl;
+        }
+
+        else{
+
+            cout<<word<<endl;
+        }
+
+    }
+
+    return 0;
+    
+}
