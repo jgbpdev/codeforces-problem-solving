c56a283135ca9fec8f933ba88f3c24010b585b1b
jgbpdev <jgbpurification@gmail.com>
2026-09-12 14:42:06 +0600

    Add files via upload

---
+++ 800/A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int n,v;
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+
+
+        cin>>v;
+
+        if(v==1){
+
+            cout<<"HARD"<<endl;
+
+            return 0;
+        }
+    }
+
+    cout<<"EASY"<<endl;
+
+
+    return 0;
+
+}
